a17ad2bc0629824516267ec5a58385eeabbce096
Cramllop01 <1791969@uab.cat>
2026-05-29 10:24:25 +0200

    Implement command line file loading for game state
    Added command line argument handling to load game state from a file.

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,22 @@ int main(int argc, const char* argv[])
 
     // The following code runs the graphic part
     Game game;
+
+    // --- CÀRREGA PER LÍNIA DE COMANDES ---
+    // Si argc és més gran que 1, vol dir que l'execució inclou un fitxer (argv[1])
+    if (argc > 1)
+    {
+        std::string filePath = argv[1];
+        if (game.load(filePath))
+        {
+            cout << "S'ha carregat l'estat inicial des de: " << filePath << std::endl;
+        }
+        else
+        {
+            cout << "No s'ha pogut carregar el fitxer: " << filePath << std::endl;
+        }
+    }
+
     // Run until ESC is pressed
     game.run();
     return 0;
